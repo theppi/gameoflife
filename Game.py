954c3a47938cf8d606733fbfa1ae158547e64b42
Bender, Thomas <thomas.bender@dialogika.de>
2024-11-07 14:59:00 +0100

    Small adjustments

--- Game.py
+++ Game.py
@@ -8,21 +8,23 @@ from Cell import Cell
 from GameField import GameField
 from State import State
 
-
+# Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 def apply_rule_underpopulation(col: Cell, living_neighbours):
     if col.state != State.ALIVE: return
     if living_neighbours < 2: col.flag = State.DEAD
 
-
+#Any live cell with two or three live neighbours lives on to the next generation.
 def apply_rule_sweetspot(col, living_neighbours):
     if col.state != State.ALIVE: return
     if living_neighbours in [2,3]: return
     col.flag = State.DEAD
 
+# Any live cell with more than three live neighbours dies, as if by overpopulation.
 def apply_rule_overpopulation(col, living_neighbours):
     if col.state != State.ALIVE: return
     if living_neighbours >3: col.flag = State.DEAD
 
+# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 def apply_rule_generation(col, living_neighbours):
     if col.state != State.DEAD: return
     if living_neighbours ==3: col.flag = State.ALIVE
@@ -40,7 +42,6 @@ class Game:
         for row_idx, row in enumerate(_map):
             for col_idx, col in enumerate(row):
                 living_neighbours = self.field.get_living_neighbours_of(row_idx, col_idx)
-                # print(f"livig ns at {col_idx},{row_idx}: {living_neighbours} ")
                 apply_rule_underpopulation(col, living_neighbours)
                 apply_rule_sweetspot(col, living_neighbours)
                 apply_rule_overpopulation(col, living_neighbours)
