cfe00790c0973cdd60b3c428fdc7e857208e437e
theppi <thomas-eppers@t-online.de>
2024-11-12 20:46:54 +0100

    Made FieldPrinter more flexible

--- FieldPrinter.py
+++ FieldPrinter.py
@@ -1,3 +1,5 @@
+import string
+
 from GameField import GameField
 from State import State
 
@@ -5,8 +7,12 @@ from State import State
 class FieldPrinter:
     character_map = {State.DEAD: "▯", State.ALIVE: "▮"}
 
-    def __init__(self, field: GameField):
+    def __init__(self, field: GameField, state_alive: string = None, state_dead: string = None):
         self.field = field
+        if state_alive is not None and len(state_alive) == 1:
+            self.character_map.update({State.ALIVE: state_alive})
+        if state_dead is not None and len(state_dead) == 1:
+            self.character_map.update({State.DEAD: state_dead})
 
     def print_field(self):
         for row in self.field.map:
